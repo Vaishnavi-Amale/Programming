85c919ac2a6db95a06f1d3919d8557cfd7c6d1ad
Vaishnavi-Amale <amalevaishnavi30@gmail.com>
2026-06-25 21:27:03 +0530

    Added programs 56 to 60

---
+++ C_Programming/Competitive/program56.c
@@ -0,0 +1,69 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Accept N numbers from user and return difference between summation of even elements and summation of odd elements
+//
+//  Input : N  : 6
+//          Elements : 85   66   3    80   93     88
+//   Output : 53    (234 - 181)
+//
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+int Difference(int Arr[], int iLength)
+{
+    int i = 0;
+    int EvenSum  = 0;
+    int OddSum = 0;
+   
+
+    for(i  = 0; i < iLength ;i++)
+    {
+        if(Arr[i] % 2 == 0)
+        {
+           EvenSum = EvenSum + Arr[i];
+        }
+        else
+        {
+           OddSum = OddSum + Arr[i];
+        }
+        
+    }
+     return EvenSum - OddSum;
+}
+
+int main()
+{
+    int iSize = 0 ,iRet = 0 , iCnt = 0 ;
+    int *p = NULL;
+   
+    
+    printf("Enter number of elements : ");
+    scanf("%d",&iSize);
+
+    p = (int *)malloc(iSize * sizeof(int));
+
+    if(p == NULL)
+    {
+        printf("Unable to allocate memory\n");
+        return -1;
+    }
+
+    printf("Enter %d elements: \n",iSize);
+
+    for(iCnt = 0;iCnt < iSize; iCnt++)
+    {
+         printf("Enter elements %d : ",iCnt+1);
+         scanf("%d",&p[iCnt]);
+    }
+
+    iRet = Difference(p,iSize);
+
+    printf("Result is  : %d\n",iRet);
+
+    free(p);
+
+    return 0;
+
+}
