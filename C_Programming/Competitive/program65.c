ad5f765628140fdef2e83b3a8ab7ff6b1e3a0509
Vaishnavi-Amale <amalevaishnavi30@gmail.com>
2026-06-26 15:47:37 +0530

    Added programs 61 to 66

---
+++ C_Programming/Competitive/program65.c
@@ -0,0 +1,66 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Accept N numbers from user and accept one another number return frequency of NO form it 
+//  Input : N  : 6
+//          NO : 66
+//          Elements : 85   66   3   66    93     88    
+//   Output : 2
+//
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int Frequency(int Arr[], int iLength ,int iNo)
+{
+   int i = 0;
+   int iCount = 0;
+
+   for(i = 0; i < iLength ; i++)
+   {
+        if(Arr[i] == iNo)
+        {
+            iCount++;
+        }
+   } 
+   return iCount; 
+}
+
+int main()
+{
+    int iSize = 0 ,iRet = 0 , iCnt = 0 ,iValue = 0;
+    int *p = NULL;
+    
+    printf("Enter number of elements : ");
+    scanf("%d",&iSize);
+
+    printf("Enter the number  : ");
+    scanf("%d",&iValue);
+
+    p = (int *)malloc(iSize * sizeof(int));
+
+    if(p == NULL)
+    {
+        printf("Unable to allocate memory\n");
+        return -1;
+    }
+
+    printf("Enter %d elements: \n",iSize);
+
+    for(iCnt = 0;iCnt < iSize; iCnt++)
+    {
+         printf("Enter elements %d : ",iCnt+1);
+         scanf("%d",&p[iCnt]);
+    }
+
+    iRet = Frequency(p, iSize, iValue);
+
+    printf("%d",iRet);
+
+    free(p);
+
+    return 0;
+
+}
